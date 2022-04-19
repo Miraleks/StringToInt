023a7aa4f1931e9ef1a7ca2fe5eaab224780b684
Alex Mironositskiy <drive_forever@mail.ru>
2022-04-19 23:03:57 +0200

    Add project files.

---
+++ StringToInt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StringToInt
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(Convert.ToInt32(S));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bad String");
+            }
+
+        }
+    }
+}
